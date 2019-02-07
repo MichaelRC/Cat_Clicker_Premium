71e74356905ce3fd2887b98bd13e4e0827f8f842
Michael R Campitiello <michaelrcampi@gmail.com>
2019-02-07 14:56:08 -0800

    Initial set up. Created JS and HTML files

---
+++ app.js
@@ -0,0 +1,19 @@
+/* App Model = Data*/
+var model = {
+
+};
+
+/* App Octopus = Function/Connected between Model & View(s)*/
+var octopus = {
+
+};
+
+/* App View for Buttons to switch cats*/
+var viewButtons = {
+
+};
+
+/* App View that shows current selected cat & click count*/
+var viewCurrentCat = {
+
+};
