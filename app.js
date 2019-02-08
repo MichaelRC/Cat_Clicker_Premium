3331ce18c5357d87c6c802ba53839f3fa3cb7c44
Michael R Campitiello <michaelrcampi@gmail.com>
2019-02-08 13:51:59 -0800

    Javascript Model added

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 /* App Model = Data*/
 var model = {
+    currentCat: null,
+    cats: [
+        {
+            name: 'Little Kitty',
+            imgSrc: 'imgs/cat_picture.jpg',
+            clicks: 0         
+        }, {
+            name: 'Soon Cat',
+            imgSrc: 'imgs/soon.jpg',
+            clicks: 0
+        }, {
+            name: 'Contemplative',
+            imgSrc: 'imgs/contemplative_cat.jpg',
+            clicks: 0
+        }, {
+            name: 'Cuddles',
+            imgSrc: 'imgs/cuddles.jpg',
+            clicks: 0
+        }, {
+            name: 'Wilde',
+            imgSrc: 'imgs/wilde.jpg',
+            clicks: 0
+        }
+    ]
 
 };
 
 /* App Octopus = Function/Connected between Model & View(s)*/
 var octopus = {
+    
 
 };
 
