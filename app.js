c7708334838a745cb81ba8a34c0243d8b9dcaa35
Michael R Campitiello <michaelrcampi@gmail.com>
2019-02-08 14:02:19 -0800

    added octopus functonality.

--- app.js
+++ app.js
@@ -30,6 +30,40 @@ var model = {
 /* App Octopus = Function/Connected between Model & View(s)*/
 var octopus = {
     
+    init: function () {
+        // Sets the currentCat to the cat in the 0 index of
+        // the 'cats' array in the view. 
+        model.currentCat = model.cats[0];
+
+        // Tells the view to initiate these functions
+        catListView.init();
+        catView.init();
+    },
+
+    // Used in Views to access the currentCat function()
+    // to get the first cat from the cats array
+    getCurrentCat: function () {
+        return model.currentCat;
+    },
+
+    // Used in Views to access the cats array
+    getCats: function () {
+        return model.cats;
+    },
+
+    // This function sets the currentCat
+    // to whatever cat is. 
+    setCurrentCat: function () {
+        model.currentCat = cat;
+    },
+
+    // When the cat is clicked, the
+    // clicks object as part of that cats 
+    // object is increased. 
+    incrementCounter: function () {
+        model.currentCat.clicks++;
+        catView.render();
+    }
 
 };
 
